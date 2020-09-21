6c7ce08f6f8f45e29e2467e99e0a79ef271c4bd4
“loribean” <aureliadotlim@gmail.com>
2020-09-22 00:17:23 +0800

    ready for deployment

---
+++ db.js
@@ -0,0 +1,35 @@
+const pg = require("pg");
+const url = require("url");
+require("dotenv").config();
+var configs;
+
+const devConfig = {
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  port: process.env.PG_PORT,
+};
+
+
+const proConfig = process.env.DATABASE_URL; //heroku addons
+
+const pool = new Pool({
+  connectionString:
+    process.env.NODE_ENV === "production" ? proConfig : devConfig,
+});
+
+pool.on("error", function (err) {
+  console.log("idle client error", err.message, err.stack);
+});
+
+const allFunction = require("./models/models");
+
+const poolRoutes = allFunction(pool);
+
+module.exports = {
+  queryInterface: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
+  pool: pool,
+  poolRoutes,
+};
